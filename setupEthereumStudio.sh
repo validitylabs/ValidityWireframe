2b8b86a965b08f0d132d4f4f07ab609a82cc6386
Sebastian C. Bürgel <SCBuergel@users.noreply.github.com>
2016-10-10 20:47:01 +0200

    Update setupEthereumStudio.sh

--- setupEthereumStudio.sh
+++ setupEthereumStudio.sh
@@ -8,6 +8,12 @@ cd
 cd workspace
 rm example-project -r
 
+# setup example project:
+mkdir course-example
+cd course-example
+mkdir contracts
+wget https://raw.githubusercontent.com/validitylabs/ValidityWireframe/master/ethereum.json
+
 # update nodejs to 6.x which is required for ipfs-api
-curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
-sudo apt-get install -y nodejs
+#curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
+#sudo apt-get install -y nodejs
