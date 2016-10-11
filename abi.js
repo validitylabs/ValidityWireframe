58cf4b7c2a02591652a361f9061e5d5ab4657aee
Sebastian Buergel <s.buergel@gmail.com>
2016-10-11 19:08:54 +0000

    added IPFS integration example

---
+++ abi.js
@@ -0,0 +1,26 @@
+contractAbi = [
+  {
+    "constant": false,
+    "inputs": [
+      {
+        "name": "s",
+        "type": "string"
+      }
+    ],
+    "name": "setGreeting",
+    "outputs": [],
+    "type": "function"
+  },
+  {
+    "constant": true,
+    "inputs": [],
+    "name": "getGreeting",
+    "outputs": [
+      {
+        "name": "s",
+        "type": "string"
+      }
+    ],
+    "type": "function"
+  }
+]
