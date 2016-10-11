f06c802e78fe5797335a982860d0825a225fdbe2
Sebastian Buergel <s.buergel@gmail.com>
2016-10-11 17:08:37 +0000

    added nameReg

---
+++ contracts/05-nameRegistry.sol
@@ -0,0 +1,23 @@
+contract nameRegistry {
+    mapping (address => string) addrReg;
+    mapping (string => address) nameReg;
+    
+    function register(string name) {
+        
+        // each name and address can only be registered once
+        if(nameReg[name] != 0 || bytes(addrReg[msg.sender]).length != 0)
+            throw;
+        
+        // registered names need to be minimally 3 characters long
+        if(bytes(name).length < 3)
+            throw;
+            
+        nameReg[name] = msg.sender;
+        addrReg[msg.sender] = name;
+    }
+    
+    function unregister() {
+        delete nameReg[addrReg[msg.sender]];
+        delete addrReg[msg.sender];
+    }
+}
